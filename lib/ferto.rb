ac4347dec5f02a1c4ed260696aa0bfb1e6ded531
Konstantinos Kostis <kkostis@skroutz.gr>
2019-05-08 17:53:16 +0300

    Introduce Ferto::ResponseError

--- lib/ferto.rb
+++ lib/ferto.rb
@@ -16,4 +16,25 @@ module Ferto
   }.freeze
 
   class ConnectionError < StandardError; end
+  
+  # A custom error class for 40X and 50X responses
+  class ResponseError < StandardError
+    
+    # Initialize a Ferto::ResponseError
+    #
+    # @param [String] err A string describing the error occured
+    # @param [Curl::Easy | nil] response a Curl::Easy object
+    #   that represents the response returned by the download method.
+    #   Default: nil
+    def initialize(err, response=nil)
+      super(err)
+      @response = response
+    end
+
+    # response is set, during the download in case of
+    # 40X or 50X responses are returned, so that it
+    # can be used in case of debugging but it is also 
+    # included for reasons of completeness.
+    attr_reader :response
+  end
 end
